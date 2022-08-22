21b21c5dac4d0cc42413a25beda8e4af1443df72
Jay Rabari <110460429+21cs053jay@users.noreply.github.com>
2022-08-22 09:32:43 +0530

    Add files via upload

---
+++ Voting_System/volunteering.js
@@ -0,0 +1,9 @@
+function func(){
+var str = document.getElementById("stream").value;
+if(str == "CSPIT-CSE"){
+    window.alert("You are a CSE student");
+}
+else if(str == "CSPIT-CE"){
+    window.alert("You are a CE student");
+}
+}
