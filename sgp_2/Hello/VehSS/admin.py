c32649bfa569101d6127cff6ca41bf2c9820a712
Aakash1509 <96115315+Aakash1509@users.noreply.github.com>
2023-04-04 15:07:13 +0530

    Delete sgp_2/Hello directory

--- sgp_2/Hello/VehSS/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Owner
-# from .models import Traveller
-# from .models import Manager
-# from .models import Traveller
-
-admin.site.register(Owner)
